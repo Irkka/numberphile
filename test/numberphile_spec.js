f01a35bd15f357f2b08f1dc9b153fee935fc6a94
Ilkka Hakkarainen <ilkka.hakkarainen@cs.helsinki.fi>
2016-10-13 13:35:04 +0300

    Enforce Node.js naming conventions

--- test/numberphile_spec.js
+++ test/numberphile_spec.js
@@ -1,16 +1,16 @@
 const assert = require('assert'),
-	Numberphile = require('../lib/numberphile')
+  Numberphile = require('../lib/numberphile')
 
 describe('Numberphile', function () {
-	describe('#is_happy', function () {
-		it('should return true, if the number is happy', function () {
-			let happy_number = 7
-			assert(Numberphile.is_happy(happy_number))
-		})
+  describe('#isHappy', function () {
+    it('should return true, if the number is happy', function () {
+      let happyNumber = 7
+      assert(Numberphile.isHappy(happyNumber))
+    })
 
-		it('should return false, if the number is unhappy', function () {
-			let unhappy_number = 8
-			assert(!Numberphile.is_happy(unhappy_number))
-		})
-	})
+    it('should return false, if the number is unhappy', function () {
+      let unhappyNumber = 8
+      assert(!Numberphile.isHappy(unhappyNumber))
+    })
+  })
 })
